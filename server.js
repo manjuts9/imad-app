d122c483f09b21f072d5bf1f83d3359f7cab4a64
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-08 00:50:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req, res){
     //articleName -- article-one
     //articles[articleName] -- {} content object for article one
-    var articleName = req.params.artcileName;
+    var articleName = req.params.aritcleName;
     res.send(createTemplate(articles[articleName]));  
 });
 
