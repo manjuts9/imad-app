94c90a74be906e454346d8fb4e5fab5e9478ca98
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-08 21:59:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 // change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'Killer1';
+
+//Move the image
+var img = document.getElementById('img');
+img.onclick = function(){
+  img.style.marginLeft = '100px';  
+};
