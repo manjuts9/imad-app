7721dbd0512cbd80d6ce12fad482fe3e8c687cfa
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-09 23:58:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,13 +28,30 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
   //make request to the server and send  name
+  var request = new XMLHttpRequest();
   
-  //capture the list of names and render as list
-  var names = ['name1', 'name2', 'name3','name4'];
-  var list = '';
-  for(var i=0;i<names.length;i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+  
+  //caputre the response and store it in variable
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status===200){
+            //capture the list of names and render as list
+         var names = request.responseText;
+         names = JSON.parse(names);
+         var list = '';
+          for(var i=0;i<names.length;i++){
+            list += '<li>' + names[i] + '</li>';
+             }
+             var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+            
+        }
+    }  
+    //not done yet
+  };
+  //make request
+  request.open('GET', 'http://manjuts90.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
+  
 };
