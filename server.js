c3788b82e392fff1d9db60e4ba08fca60c5c0204
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-12 18:13:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool = require('pg').Pool;
 var config = {
  user:'manjuts90',
  database:'manjuts90',
- host:'db.imad.hasura_app.io',
+ host:'db.imad.hasura-app.io',
  port:'5432',
  password:process.env.DB_PASSWORD
 };
