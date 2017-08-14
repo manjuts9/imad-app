0ada494bc022e6050beb5dda010e791cb8be422b
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-15 00:23:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
  user:'manjuts90',
@@ -13,7 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 function createTemplate (data) {
 var title = data.title;
@@ -60,9 +61,27 @@ function  hash (input, salt){
     var hashed = crypto.pbkdf2Sync(input,salt,1000,500,'sha512');
     return ['pbkdf','1000', salt, hashed.toString('hex')].join('$');
 }
+
 app.get('/hash/:input', function(req,res){
    var hashedString = hash (req.params.input, 'this-is-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user', function(req,res) {
+    //usernamr and password
+    var username =req.body.username;
+    var password =req.body.password;
+    
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    
+pool.query('INSERT INTO "USER" (username, password) VALUES ($1,$2)', [username, dbString], function (err,result){
+         if(err){
+         res.status(500).send(err.toString());
+         }else {
+         res.send('user sucessfully created:' +username);
+         }
+     });
 });
 
 var pool = new Pool(config);
