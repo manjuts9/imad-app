980ca6946f2782601515c1fa6e9b9b81ac9bf5ce
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-14 23:39:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ app.get('/', function (req, res) {
 
 function  hash (input, salt){
     //how to create hash
-    var hashed = crypto.pbkd2Sync(input,salt,1000,500,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,1000,500,'sha512');
     return hashed.toString('hex');
 }
-app.get('/hash/input', function(req,res){
-   var hashedString = hash (req.params,input, 'this-is-random-string');
+app.get('/hash/:input', function(req,res){
+   var hashedString = hash (req.params.input, 'this-is-random-string');
    res.send(hashedString);
 });
 
