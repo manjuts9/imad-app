c3644436ea575a17a04b5a64922aa892e3904c2e
manjuts9 <30662999+manjuts9@users.noreply.github.com>
2017-08-15 19:45:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ pool.query('SELECT * FROM "USER" WHERE username = $1', [username], function (err
     
 });
 
-app.get('check-login', function(req, res){
+app.get('/check-login', function(req, res){
    if(req.session && req.session.outh && req.session.outh.userId){
        req.send('you are logged in:' + req.session.outh.userID.toString());
    } else{
